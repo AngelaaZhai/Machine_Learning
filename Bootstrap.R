2de648f7be5446b4beb465310ecd69dd805f3526
Angela Zhai <angelazhai@Angelas-MacBook-Pro.local>
2019-01-15 22:45:53 -0500

    Bootstrap

---
+++ Bootstrap.R
@@ -0,0 +1,22 @@
+## Bootstrap
+## Minimum risk investment
+alpha = function(x, y){
+  vx = var(x)
+  vy = var(y)
+  cxy = cov(x, y)
+  (vy-cxy)/(vx+vy-2*cxy)
+}
+alpha(airbnb$price, airbnb$reviews)
+
+## What is the standard error of alpha?
+alpha.fn = function(data, index){
+  with(data[index, ], alpha(price, reviews))
+}
+alpha.fn(airbnb, 1:100)
+
+set.seed(1)
+alpha.fn(airbnb, sample(1:100, 100, replace=TRUE))
+
+boot.out = boot(airbnb, alpha.fn, R=1000)
+boot.out
+plot(boot.out)
